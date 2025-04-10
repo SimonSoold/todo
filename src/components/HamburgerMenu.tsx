e4cf7bbb21fb3b11ce5b1b0ce1f4674011bf4dd2
Simon Soold <soold.simon@gmail.com>
2025-04-11 01:24:19 +0200

    logout

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { 
+  useDispatch
+} from 'react-redux';
+import { 
+  logout
+} from '../redux/userSlice';
+import { 
+  useNavigate
+} from 'react-router';
 export const ThemeMenu: React.FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login")
+  };
   return (
     <details>
         <summary>
@@ -7,7 +22,10 @@ export const ThemeMenu: React.FC = () => {
         </summary>
         <ul>
             <li>Settings</li>
-            <li>Logout</li>
+            <li
+              onClick={handleLogout}
+            >
+              Logout</li>
         </ul>
     </details>
   )
